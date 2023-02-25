235077612d670294650f7a31f7a1c588aae4dc57
hannah <hgsherid@uvm.edu>
2023-02-25 18:15:11 -0500

    testing Player class

--- testing.cpp
+++ testing.cpp
@@ -5,9 +5,19 @@
 #include "Player.h"
 #include <vector>
 bool testWheelOfFortune();
+bool testPlayer();
+bool testWheel();
 
 int main() {
-    testWheelOfFortune();
+    if (testWheelOfFortune()) {
+        cout << "PASSED ALL WHEEL OF FORTUNE TEST CASES" << endl;
+    }
+    if (testPlayer()) {
+        cout << "PASSED ALL PLAYER TEST CASES" << endl;
+    }
+    if (testWheel()) {
+        cout << "PASSED ALL WHEEL TEST CASES" << endl;
+    }
     return 0;
 }
 /**
@@ -39,8 +49,84 @@ bool testWheelOfFortune() {
 bool testPlayer() {
     bool passed = true;
 
-    // 
+    // Default constructor
+    Player player1;
+    if (player1.getName() != "Current Player") {
+        cout << "FAILED Player default constructor test case" << endl;
+        passed = false;
+    }
+    if (player1.getBalance() != 0 || player1.getNumWins() != 0) {
+        cout << "FAILED Player default constructor test case 2" << endl;
+        passed = false;
+    }
+    if (player1.getPrize() != 200) {
+        cout << "FAILED default constructor test case 3" << endl;
+        passed = false;
+    }
 
+    // Constructor
+    Player player2("Hannah");
+    if (player2.getName() != "Hannah") {
+        cout << "FAILED Player constructor test case 1" << endl;
+        passed = false;
+    }
+
+    if (player2.getBalance() != 0 || player2.getNumWins() != 0) {
+        cout << "FAILED Player constructor test case 2" << endl;
+        passed = false;
+    }
+    if (player2.getPrize() != 200) {
+        cout << "FAILED Player constructor test case 3" << endl;
+        passed = false;
+    }
+
+    // Guess consonant
+    player1.setPrize(100);
+
+    player1.guessConsonant(3);
+    if (player1.getBalance() != 300) {
+        cout << "FAILED guess consonant test case 1" << endl;
+        passed = false;
+    }
+
+    // Buy vowel
+    player1.buyVowel();
+    if (player1.getBalance() != 50) {
+        cout << "FAILED buy vowel test case 1" << endl;
+        passed = false;
+    }
+
+    // Bankrupt
+    player1.bankrupt();
+    if (player1.getBalance() != 0) {
+        cout << "FAILED bankrupt test case" << endl;
+        passed = false;
+    }
+
+    // Sufficient funds
+    if (player1.sufficientFunds()) {
+        cout << "FAILED sufficient funds test case" << endl;
+        passed = false;
+    }
+
+    player1.setName("Jane Doe");
+    player2.setName("Jane Doe");
+    // Overloaded == operator
+    if (!(player1 == player2)) {
+        cout << "FAILED overloaded == operator test case" << endl;
+        passed = false;
+    }
+
+    player2.setName("John Doe");
+    if (player1 == player2) {
+        cout << "FAILED overloaded == operator" << endl;
+        passed = false;
+    }
+
+    // Overloaded << operator
+    cout << "Testing Player overloaded print operator..." << endl;
+    cout << "Player 1: " << player1 << endl << "Player 2: " << player2 << endl;
+    cout << "End testing Player overloaded print operator" << endl;
 
     return passed;
 }
